1907a460c274fa590ff9f2a616ea3ce3c083f7dc
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-21 14:42:00 +0530

    .

--- 2nd.cpp
+++ 2nd.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main() {
-    int a = 10, b = 5;
+    int a, b;
+    cout << "Enter 1st number";
+    cin >> a;
+    cout << "Enter 2nd number";
+    cin >> b;
 
     cout << "sum = " << (a+b) << endl;
     cout << "difference = " << (a-b) << endl;
     cout << "product = " << (a*b) << endl;
     cout << "division = " << (a/b) << endl;
+    cout << "modulo = " << (a%b) << endl; //remainder
     return 0;
 }
