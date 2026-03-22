82e4b045afe76185a5290951279abd6baa835f5c
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-22 15:33:47 +0530

    Create 4.cpp

---
+++ L2/4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 9;
+    
+    if(n%3==0){
+        cout<<"divisible";
+    }else{
+        cout<<"not divisible";
+    }
+    
+
+}
