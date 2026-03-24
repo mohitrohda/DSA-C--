b78f1d1253f857bd8d8a35820c68c9e21b6b1fb6
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-24 16:26:13 +0530

    Create 4.cpp

---
+++ L4_patterns/4.cpp
@@ -0,0 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+//     char ch = 'A';
+
+//     for(int i=0; i<n; i++) {
+        
+//         for(int j=0; j<n; j++) {
+//             cout << ch;
+//             ch = ch + 1;
+//         }
+//     cout << endl ;
+//     }
+
+
+//   return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    int num = 1;
+
+    for(int i=0; i<n; i++) {
+        
+        for(int j=0; j<n; j++) {
+            cout << num << " ";
+            num = num + 1;
+        }
+    cout << endl ;
+    }
+
+
+  return 0;
+}
