0db06ec0954adf180e649da1be8212d9a76f3feb
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-26 21:27:11 +0530

    Create 5hw.cpp

---
+++ L5_functions/5hw.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int fibbonaci(int n) {
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    int a = 0, b = 1, c;
+
+    for(int i=2; i<=n; i++) {
+        c = a +b;
+        a = b;
+        b = c;
+    }
+    return b;
+
+}
+
+int main() {
+    int n;
+    cout << "Enter Number : ";
+    cin>>n;
+
+    cout << "Nth Fibbonacci = "<< fibbonaci(n) << endl;
+    
+    return 0;
+}
