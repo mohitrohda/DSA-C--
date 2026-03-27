c4fa701f3b08b59df33ac23133b7a631be88b3ba
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-27 13:16:09 +0530

    Create 1.cpp

---
+++ L7_Bitwise/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << (6 & 10) << endl; //2
+    cout << (6 | 10) << endl; //14
+    cout << (6 ^ 10) << endl; //
+
+    cout << (10 << 2) << endl; //40
+    cout << (10 >> 1) << endl; //5
+
+    return 0;
+}
