aa4c218b7e44123d5dbece29199a898340ec295d
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-03-28 13:28:54 +0530

    Create 8hw.cpp

---
+++ L8_Array/8hw.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int uniqu_val(int arr[], int size) {
+    int start = 0;
+
+    for(int i = 0; i < size; i++) {
+        
+    }
+
+}
+
+int main() {
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size = 7;
+
+    return 0;
+}
