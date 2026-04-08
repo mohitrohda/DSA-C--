461823749402510198d26ecdbe97c2bb986ebc32
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-04-08 21:58:07 +0530

    Create 4.cpp

---
+++ L11_majority+pairsum/4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        //sort
+        sort(nums.begin(), nums.end());
+
+        //freq count
+        int freq = 1, ans = nums[0];
+
+        for(int i =1; i<n; i++) {
+            if(nums[i]==nums[i-1]) {
+                freq++;
+            } else{
+                freq = 1;
+                ans = nums[i];
+            }
+
+            if(freq > n/2) {
+                return ans;
+            }
+        }
+        
+        return ans;
+    }
+};
